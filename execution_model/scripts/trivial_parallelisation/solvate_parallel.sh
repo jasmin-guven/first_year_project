dc8d39e63a556453e7120fbe3c874f062bb661dd
jasmin-guven <jasmin.guven@hotmail.com>
2022-05-26 21:52:31 +0100

    try new script

--- execution_model/scripts/trivial_parallelisation/solvate_parallel.sh
+++ execution_model/scripts/trivial_parallelisation/solvate_parallel.sh
@@ -6,9 +6,9 @@ N_TASKS=$(($N_LIGANDS -1))
 
 for i in $(seq 0 $N_TASKS)
 do
-
-cd ligands/ligand_$i
-nohup python solvation_$i.py $i > solv.out &
+DIR_INDEX=$(($i +1))
+cd ligands/ligand_$DIR_INDEX
+#nohup python solvation_$i.py $i > solv.out &
 cd ../../
 
 done
