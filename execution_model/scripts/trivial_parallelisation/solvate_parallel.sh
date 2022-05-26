61df57f27ab3d0243329bc5e4e3d3dfebdb9ccfc
jasmin-guven <jasmin.guven@hotmail.com>
2022-05-26 21:36:38 +0100

    correct files for parallel solvation

--- execution_model/scripts/trivial_parallelisation/solvate_parallel.sh
+++ execution_model/scripts/trivial_parallelisation/solvate_parallel.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-DIRS=$(ls -l ligands | grep ^d | wc -l)
+N_LIGANDS=$(ls -l ligands | grep ^d | wc -l)
 
-IDS = 
+N_TASKS = $(($N_LIGANDS -1))
 
-for i in $(seq 1 $DIRS)
+for i in $(seq 0 $N_TASKS)
 do
+
 cd ligands/ligand_$i
-nohup python solvation_$i.py  > solv.out &
+nohup python solvation_$i.py $i > solv.out &
 cd ../../
+
 done
