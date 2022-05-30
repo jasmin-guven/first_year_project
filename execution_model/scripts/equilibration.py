2f5a7d6cc01aeb18024dd87b8df451c9eec8e34d
jasmin-guven <jasmin.guven@hotmail.com>
2022-05-30 12:29:30 +0100

    fixed mistakes with tqdm and saving mdrun output

--- execution_model/scripts/equilibration.py
+++ execution_model/scripts/equilibration.py
@@ -2,25 +2,25 @@ import BioSimSpace as bss
 import sys
 from BioSimSpace import _Exceptions
 import os
-from tqdm import tqdm
+
 
 def run_process(system, md_protocol):
     """
     @param system: solvated system (bss object)
     @param md_protocol: bss protocolbss protocol
-    @return the processed system.
+    @return the process.
     """
     process = bss.Process.Gromacs(system, md_protocol)
     process.setArg("-nt", 1)
-    print(process.workDir())
     process.start()
-    process.wait()
+    process.wait()    
+    print(f"\n {process}")
+    print(f"\n Runtime: {process.runTime()}")
     if process.isError():
         print(process.stdout())
         print(process.stderr())
         raise _Exceptions.ThirdPartyError("The process exited with an error!")
-    system = process.getSystem()
-    return system
+    return process
 
 
 minimisation_steps = 500
@@ -31,6 +31,9 @@ runtime_npt = 200  # ps
 print(f"program: {sys.argv[0]}, index: {sys.argv[1]}")
 index = int(sys.argv[1])
 
+os.system(f"mkdir -p ../equilibration_files/ligands/")
+os.system(f"mkdir -p ../equilibration_files/protein/")
+
 ligand_stream = open("../ligands.dat", "r")
 ligand_lines = ligand_stream.readlines()
 ligand_name = ligand_lines[index].rstrip()
@@ -39,59 +42,98 @@ ligand_solvated = bss.IO.readMolecules([f"../inputs/ligands/{ligand_name}_ligand
                                         f"../inputs/ligands/{ligand_name}_ligand_solvated.rst7"])
 system_solvated = bss.IO.readMolecules([f"../inputs/ligands/{ligand_name}_system_solvated.prm7",
                                         f"../inputs/ligands/{ligand_name}_system_solvated.rst7"])
+
 print("---------------------------")
 print("Working on solvated ligand.")
-print(f"Minimising in {minimisation_steps} steps.")
+
+print(f"\nMinimising in {minimisation_steps} steps.")
 protocol = bss.Protocol.Minimisation(steps=minimisation_steps)
-for i in tqdm(range(minimisation_steps)):
-    minimised = run_process(ligand_solvated, protocol)
+minimised_process = run_process(ligand_solvated, protocol)
+
+output_files = minimised_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/ligands/minimisation_{index}")
+
+minimised = minimised_process.getSystem()
+
 print("Finished minimisation.")
 
-print(f"NVT equilibration for {runtime_short_nvt} ps while restraining all non-solvent atoms.")
+
+print(f"\nNVT equilibration for {runtime_short_nvt} ps while restraining all non-solvent atoms.")
 protocol = bss.Protocol.Equilibration(
                                     runtime=runtime_short_nvt*bss.Units.Time.picosecond,
                                     temperature_start=0*bss.Units.Temperature.kelvin,
                                     temperature_end=300*bss.Units.Temperature.kelvin,
                                     restraint="all"
                                 )
-for i in tqdm(range(runtime_short_nvt)):
-    restrained_nvt = run_process(minimised, protocol)
+restrained_nvt_process = run_process(minimised, protocol)
 
-print(f"NVT equilibration for {runtime_nvt} ps without restraints.")
+output_files = restrained_nvt_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/ligands/restrained_nvt_{index}")
+
+restrained_nvt = restrained_nvt_process.getSystem()
+
+print("Finished restrained NVT.")
+
+print(f"\nNVT equilibration for {runtime_nvt} ps without restraints.")
 protocol = bss.Protocol.Equilibration(
                                 runtime=runtime_nvt*bss.Units.Time.picosecond,
                                 temperature=300*bss.Units.Temperature.kelvin,
                                 )
-for i in tqdm(range(runtime_nvt)):
-    nvt = run_process(restrained_nvt, protocol)
+nvt_process = run_process(restrained_nvt, protocol)
 
-print(f"NPT equilibration for {runtime_npt} ps while restraining non-solvent heavy atoms.")
+output_files = nvt_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/ligands/nvt_{index}")
+
+nvt = restrained_nvt_process.getSystem()
+
+print("Finished unrestrained NVT.")
+
+
+print(f"\nNPT equilibration for {runtime_npt} ps while restraining non-solvent heavy atoms.")
 protocol = bss.Protocol.Equilibration(
                                 runtime=runtime_npt*bss.Units.Time.picosecond,
                                 pressure=1*bss.Units.Pressure.atm,
                                 temperature=300*bss.Units.Temperature.kelvin,
                                 restraint="heavy",
                                 )
-for i in tqdm(range(runtime_npt)):
-    restrained_npt = run_process(nvt, protocol)
+restrained_npt_process = run_process(nvt, protocol)
 
-print(f"NPT equilibration for {runtime_npt} ps without restraints.")
+output_files = restrained_npt_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/ligands/restrained_npt_{index}")
+
+restrained_npt = restrained_npt_process.getSystem()
+
+print("Finished restrained NPT.")
+
+print(f"\nNPT equilibration for {runtime_npt} ps without restraints.")
 protocol = bss.Protocol.Equilibration(
                                 runtime=runtime_npt*bss.Units.Time.picosecond,
                                 pressure=1*bss.Units.Pressure.atm,
                                 temperature=300*bss.Units.Temperature.kelvin,
                                 )
 
-for i in tqdm(range(runtime_npt)):
-    equilibrated_ligand = run_process(restrained_npt, protocol)
+npt_process = run_process(restrained_npt, protocol)
+
+output_files = npt_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/ligands/npt_{index}")
+
+equilibrated_ligand = npt_process.getSystem()
+
+print("Finished equilibrating ligand.")
 
 print("---------------------------")
 print("Working on solvated ligand+protein.")
-print(f"Minimising in {minimisation_steps} steps..")
-protocol = bss.Protocol.Minimisation(steps=minimisation_steps)
 
-for i in tqdm(range(minimisation_steps)):
-    minimised = run_process(system_solvated, protocol)
+print(f"\nMinimising in {minimisation_steps} steps..")
+protocol = bss.Protocol.Minimisation(steps=minimisation_steps)
+minimised_system_process = run_process(system_solvated, protocol)
+
+output_files = minimised_system_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/protein/minimisation_{index}")
+
+minimised_system = minimised_system_process.getSystem()
+
+print("Finished minimisation.")
 
 print(f"NVT equilibration for {runtime_short_nvt} ps while restraining all non-solvent atoms.")
 protocol = bss.Protocol.Equilibration(
@@ -100,9 +142,14 @@ protocol = bss.Protocol.Equilibration(
                                 temperature_end=300*bss.Units.Temperature.kelvin,
                                 restraint="all"
                                 )
+restrained_nvt_system_process = run_process(minimised, protocol)
 
-for i in tqdm(range(runtime_short_nvt)):
-    restrained_nvt_system = run_process(minimised, protocol)
+output_files = restrained_nvt_system_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/protein/restrained_nvt_{index}")
+
+restrained_nvt_system = restrained_nvt_system_process.getSystem()
+
+print("Finished restrained NVT.")
 
 print(f"NVT equilibration for {runtime_nvt} ps while restraining all backbone atoms.")
 protocol = bss.Protocol.Equilibration(
@@ -110,17 +157,28 @@ protocol = bss.Protocol.Equilibration(
                                 temperature=300*bss.Units.Temperature.kelvin,
                                 restraint="backbone"
                                 )
+backbone_restrained_nvt_process = run_process(restrained_nvt_system, protocol)
 
-for i in tqdm(range(runtime_nvt)):
-    backbone_restrained_nvt_system = run_process(restrained_nvt_system, protocol)
+output_files = backbone_restrained_nvt_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/protein/backbone_restrained_nvt_{index}")
+
+backbone_restrained_nvt = backbone_restrained_nvt_process.getSystem()
+
+print("Finished backbone-restrained NVT.")
 
 print(f"NVT equilibration for {runtime_nvt} ps without restraints.")
 protocol = bss.Protocol.Equilibration(
                                 runtime=runtime_nvt*bss.Units.Time.picosecond,
                                 temperature_end=300*bss.Units.Temperature.kelvin,
                                 )
-for i in tqdm(range(runtime_nvt)):
-    nvt_system = run_process(backbone_restrained_nvt_system, protocol)
+nvt_system_process = run_process(backbone_restrained_nvt, protocol)
+
+output_files = nvt_system_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/protein/nvt_{index}")
+
+nvt_system = nvt_system_process.getSystem()
+
+print("Finished NVT.")
 
 print(f"NPT equilibration for {runtime_npt} ps while restraining non-solvent heavy atoms..")
 protocol = bss.Protocol.Equilibration(
@@ -129,9 +187,14 @@ protocol = bss.Protocol.Equilibration(
                                 temperature=300*bss.Units.Temperature.kelvin,
                                 restraint="heavy",
                                 )
+restrained_npt_system_process = run_process(nvt_system, protocol)
 
-for i in tqdm(range(runtime_npt)):
-    restrained_npt_system = run_process(nvt_system, protocol)
+output_files = restrained_npt_system_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/protein/restrained_npt_{index}")
+
+restrained_npt_system = restrained_npt_system_process.getSystem()
+
+print("Finished restrained NPT.")
 
 print(f"NPT equilibration for {runtime_npt} ps without restraints.")
 protocol = bss.Protocol.Equilibration(
@@ -139,9 +202,15 @@ protocol = bss.Protocol.Equilibration(
                                 pressure=1*bss.Units.Pressure.atm,
                                 temperature=300*bss.Units.Temperature.kelvin,
                                 )
-for i in tqdm(range(runtime_npt)):
-    equilibrated_system = run_process(restrained_npt_system, protocol)
+npt_system_process = run_process(restrained_npt_system, protocol)
 
+output_files = npt_system_process.workDir()
+os.system(f"cp -r {output_files} ../equilibration_files/protein/npt_{index}")
+
+equilibrated_system = npt_system_process.getSystem()
+
+print("Finished equilibrating ligand-protein system.")
+    
 os.system("mkdir -p ../prep/ligands")
 os.system("mkdir -p ../prep/protein")
 
