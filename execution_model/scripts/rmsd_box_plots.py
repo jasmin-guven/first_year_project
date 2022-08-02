b4b00418421c81448c1b1562acbce6f02de8c929
jasmin-guven <jasmin.guven@hotmail.com>
2022-08-02 16:26:06 +0100

    bound stage rmsd plots

--- execution_model/scripts/rmsd_box_plots.py
+++ execution_model/scripts/rmsd_box_plots.py
@@ -13,9 +13,9 @@ ligands = np.delete(ligands_all, [10, 12, 13])
 times = []
 rmsds = []
 rmsd_dictionary = {}
-
+stage = "bound"
 for i in range(len(ligands)):
-    path = f"../outputs/SOMD/lig_h_1~lig_h_{ligands[i]}/free/lambda_1.0000/"
+    path = f"../outputs/SOMD/lig_h_1~lig_h_{ligands[i]}/{stage}/lambda_1.0000/"
     print(f"looking at {path}")
     trajectory_filename = ""
     if os.path.exists(path+"traj000000002.dcd"):
@@ -51,12 +51,10 @@ for i in range(len(ligands)):
     ax.plot(time, rmsd_values, "k-")
     ax.set_xlabel("time (ps)", fontsize = 14)
     ax.set_ylabel(r"RMSD ($\AA$)", fontsize = 14)
-    plt.savefig(f"../../plots/rmsd_lig_{ligands[i]}.pdf")
+    plt.savefig(f"../../plots/rmsd/{stage}/rmsd_lig_{ligands[i]}.pdf")
 
     rmsd_dictionary["time"] = times
     rmsd_dictionary["rmsd"] = rmsds
-
-
 
 
 fig = plt.figure(figsize=(10, 10))
@@ -68,4 +66,4 @@ plt.boxplot(rmsd_dictionary["rmsd"], medianprops=median_line_properties)
 plt.xticks(ticks=xtick_positions, labels=ligands)
 plt.xlabel("Ligand")
 plt.ylabel(f"RMSD ($\AA$)")
-plt.savefig("../../plots/box_plot_rmsd.pdf")
+plt.savefig(f"../../plots/rmsd/{stage}/box_plot_rmsd_{stage}.pdf")
